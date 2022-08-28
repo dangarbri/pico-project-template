ea3215fa87fe04aafb99dc4387bf74b003be43a6
Daniel Garcia Briseno <dg.briseno@gmail.com>
2022-08-28 13:29:16 -0400

    Add sensor library example

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
+#include "distance_sensor.h"
 
 int main() {
     stdio_init_all();
+
+    DistanceSensor hcsr04{pio0, 0, 0};
+
+    while (true) {
+        hcsr04.TriggerRead();
+        while (hcsr04.is_sensing) {
+            sleep_us(10);
+        }
+        printf("Reading: %d cm\n", hcsr04.distance);
+        sleep_ms(1000);
+    }
 }
 
